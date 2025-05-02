abeaa6d3b0b48d8b8941fd55e34049d400180cba
Kauan Morinel Calheiro <kauan.calheiro@universo.univates.br>
2025-05-01 23:02:59 -0300

    adiciona constante STORAGE_ENPOINT ao arquivo api.ts

--- web/constants/api.ts
+++ web/constants/api.ts
@@ -1,3 +1,5 @@
 export const USER_ENPOINT = `/user`;
 export const MUSCLE_GROUP_ENDPOINT = `/muscle-group`;
 export const EXERCISE_ENDPOINT = `/exercise`;
+
+export const STORAGE_ENPOINT = `/storage`;
