6994855b7c053b0d28f2aeba182b53515f223d52
Kauan Morinel Calheiro <kauan.calheiro@universo.univates.br>
2025-05-01 23:00:43 -0300

    substitui componente de exclusão de usuário por BaseDeleteDialog e remove lógica de exclusão manual

--- web/app/components/user/Delete.vue
+++ web/app/components/user/Delete.vue
@@ -1,42 +1,18 @@
 <script setup lang="ts">
 import type User from "~~/types/User";
-import type { $Fetch } from "ofetch";
 import { USER_ENPOINT } from "~~/constants/api";
 
 const { user } = defineProps<{ user: User }>();
 const emit = defineEmits(["after-delete"]);
-const isOpen = defineModel<boolean>();
-
-function onDelete() {
-  const fetch: $Fetch = useSanctumClient();
-  fetch(`${USER_ENPOINT}/${user.id}`, { method: "DELETE" })
-    .then(() => {
-      isOpen.value = false;
-      emit("after-delete");
-    })
-    .catch(() => {
-      let error: any = useNuxtApp().$lastSanctumError;
-      toast().error({
-      description: error?.value?._data?.message || "Ocorreu um erro",
-    });
-    });
-}
-
-const onCancel = () => {
-  isOpen.value = false;
-};
+const open = defineModel<boolean>();
 </script>
 
 <template>
-  <BaseQuest
-    v-model="isOpen"
-    @accept="onDelete"
-    @decline="onCancel"
-    accept-text="Excluir usuário"
-    decline-text="Cancelar"
-  >
-    Confirme a exclusão do usuário <strong
-      >"{{ user.id }} - {{ user.name }}"</strong
-    >
-  </BaseQuest>
+  <BaseDeleteDialog
+    v-model:open="open"
+    :entity="user"
+    :endpoint="USER_ENPOINT"
+    entity-name="Usuário"
+    @after-delete="$emit('after-delete')"
+  />
 </template>
