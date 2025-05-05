32989c27d78a8f40d894996651b8816e37bd34d0
Kauan Morinel Calheiro <kauan.calheiro@universo.univates.br>
2025-05-05 20:07:14 -0300

    feat: reestrutura a página de configurações para utilizar UDrawers e melhorar a organização dos componentes

--- web/app/pages/settings.vue
+++ web/app/pages/settings.vue
@@ -4,19 +4,39 @@
   <div
     class="space-y-4 w-full py-5 px-5 md:px-70 md:py-10 rounded-2xl flex flex-col gap-2 mx-auto"
   >
-    <div class="flex flex-col gap-2">
-      <div class="flex flex-row gap-2 text-center">
-        <UIcon name="solar:cloud-sun-2-bold-duotone" size="1.5rem" />
-        <h1>Aparência</h1>
-      </div>
-      <ColorModeSelector />
-    </div>
-    <div class="flex flex-col gap-2">
-      <div class="flex flex-row gap-2 text-center">
-        <UIcon name="solar:palette-round-bold-duotone" size="1.5rem" />
-        <h1>Cores</h1>
-      </div>
-      <ColorPicker />
-    </div>
+    <h1 class="text-2xl font-bold">Configurações</h1>
+    <UDrawer class="">
+      <UButton
+        color="neutral"
+        class="w-full px-4 py-4 justify-center"
+        size="xl"
+        icon="solar:cloud-sun-2-bold"
+      >
+        <span class="text-sm">Aparência</span>
+      </UButton>
+
+      <template #body>
+        <div class="min-h-[50dvh]">
+          <ColorModeSelector
+            class="min-h-[50dvh] m-auto flex flex-col justify-center"
+          />
+        </div>
+      </template>
+    </UDrawer>
+
+    <UDrawer>
+      <UButton
+        color="neutral"
+        class="w-full px-4 py-4 justify-center"
+        size="xl"
+        icon="solar:palette-round-bold-duotone"
+      >
+        <span class="text-sm">Cores</span>
+      </UButton>
+
+      <template #body>
+        <ColorPicker />
+      </template>
+    </UDrawer>
   </div>
 </template>
